0c2fa5f6f1ac107f9082c86b49ff44f5c3be1079
Nellly456 <123640035+Nellly456@users.noreply.github.com>
2025-03-18 21:21:15 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,12 +16,11 @@ st.set_page_config(
     page_icon="🏃",
     layout="wide"
 )
-
+url = "https://giuzfwdleogsfpsnphgs.supabase.co"
+    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpdXpmd2RsZW9nc2Zwc25waGdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5Mzc3MzMsImV4cCI6MjA1NDUxMzczM30.EIAv7aIeILgv-9AVGA1jff_aM4stJ8nYCTwOghvfv_M"
 # Supabase connection
 @st.cache_resource
 def init_connection():
-    url = "https://giuzfwdleogsfpsnphgs.supabase.co"
-    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpdXpmd2RsZW9nc2Zwc25waGdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5Mzc3MzMsImV4cCI6MjA1NDUxMzczM30.EIAv7aIeILgv-9AVGA1jff_aM4stJ8nYCTwOghvfv_M"
     return create_client(url, key)
 
 # Fetch data from Supabase
